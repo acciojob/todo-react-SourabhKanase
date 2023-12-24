fb220b34a614d20a92d70a63c5a791644bea0770
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-12-24 10:52:27 +0000

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+// import { valuesIn } from "cypress/types/lodash";
+// import { values } from "cypress/types/lodash";
 
 const App = () => {
+  let [values,setvalue]=useState("");
+  let [AllTodoValues,setAllTodoValues]=useState([]);
+  let [dynamicId,setdynamicId]=useState(1);
+
+  function enterTodo(e){
+    setvalue(e.target.value);
+  }
+  function AddTodo(e)
+  {
+       e.preventDefault();
+      setAllTodoValues([...AllTodoValues,{id:dynamicId,tittle:values}]);
+      setdynamicId(dynamicId+1);
+      setvalue("");
+  }
+
+  function deleteitem(id)
+  {
+       setAllTodoValues(AllTodoValues.filter((item)=>{
+          if(item.id!=id)
+          {
+            return item;
+          }
+       }))
+  }
   return (
+    
     <div>
-        {/* Do not remove the main div */}
+        <form onSubmit={AddTodo}>
+          <input type="text" placeholder="enter todo" onChange={enterTodo} value={values}/>
+          <button type="submit">Add Task</button>
+        </form>
+        <ul>
+         {
+           AllTodoValues.map((item)=>{
+            return <li>
+              <p>{item.tittle}</p>
+              <button onClick={()=>{deleteitem(item.id)}}>Delete</button>
+            </li>
+           })
+         }
+        </ul>
     </div>
   )
 }
